2a76003d7631c27b0c3349a86c6d45e845d82086
Heinti <106119626+Heinti@users.noreply.github.com>
2022-11-19 17:23:27 +0000

    Deploying to gh-pages from @ Heinti/goit-js-hw-09@762f35d9b53a98647d814390669b4461815a3afc 🚀

---
+++ 01-color-switcher.a8a90d44.js
@@ -0,0 +1,2 @@
+document.querySelector('button[data-start="start"]'),document.querySelector('button[data-stop="stop"]');
+//# sourceMappingURL=01-color-switcher.a8a90d44.js.map
---
+++ 01-color-switcher.e175a651.js
@@ -0,0 +1,2 @@
+document.querySelector('button[data-start="start"]'),document.querySelector('button[data-stop="stop"]');
+//# sourceMappingURL=01-color-switcher.e175a651.js.map
